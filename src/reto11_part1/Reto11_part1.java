3d358e7fd90524dd92fe61fe6926c8bddeeb759d
jean98763 <jean98763_19@hotmail.com>
2019-05-10 13:29:53 -0500

    parte 1 programa comida

---
+++ src/reto11_part1/Reto11_part1.java
@@ -0,0 +1,24 @@
+
+package reto11_part1;
+
+
+public class Reto11_part1 {
+
+   
+    public static void main(String[] args) {
+       String nombre;
+       
+    }
+       private int nivelSabor;
+       private int nivelPicante;
+       private String [] ingredientes = new String[0];
+       
+       public static void mostrarIngredientes(){
+       
+       }
+       
+       public static boolean  espicante(){
+        return false;
+       
+       } 
+}
